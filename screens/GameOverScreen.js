d937de9686286b12af6c8a03c9c8db1518dd5019
mensafri <safrijawad1245@gmail.com>
2023-09-23 21:19:30 +0700

    add some screen and component

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,5 @@
+export default function GameOverScreen() {
+  return (
+    <div>GameOverScreen</div>
+  )
+}
---
+++ screens/GameScreen.js
@@ -0,0 +1,5 @@
+export default function GameScreen() {
+  return (
+    <div>GameScreen</div>
+  )
+}
