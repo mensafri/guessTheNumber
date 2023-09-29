a69a71d2bcd19e66aadb8309e9d4584dfed00cb8
mensafri <safrijawad1245@gmail.com>
2023-09-30 05:38:33 +0700

    set game over

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import { Alert, StyleSheet, Text, View } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
@@ -17,13 +17,19 @@ function generateRandomBetween(min, max, exclude) {
 let minBoundary = 1;
 let maxBoundary = 100;
 
-export default function GameScreen({ userNumber }) {
+export default function GameScreen({ userNumber, onGameOver }) {
 	const initialGuess = generateRandomBetween(
-		minBoundary,
-		maxBoundary,
+		1,
+		100,
 		userNumber
 	);
 	const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+	useEffect(() => {
+		if (currentGuess == userNumber) {
+			onGameOver();
+		}
+	}, [currentGuess, userNumber, onGameOver]);
 
 	function nextGuessNumber(direction) {
 		if (
